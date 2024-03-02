44d08ddd6a40da0e51fcbfd0d71ce2b1343cba75
Vika0408 <palvanshika609@gmail.com>
2024-03-02 20:00:02 +0530

    changes

--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .froms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
-def home(request) :
-    #return HttpResponse('Hello world!')
+def home(request):
+    # return HttpResponse('Hello world!')
     return render(request, 'crud/index.html')
+
+# Register a user
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+    context = {'form' : form}
+    return render(request, 'crud/register.html', context=context)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crud.apps.CrudConfig',
+
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
